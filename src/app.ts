30f5eeef8e6d6a5ac3772e8bc4e14e70723d04ad
Andy0414 <pjh8667@naver.com>
2020-07-14 20:06:11 +0900

    Update express & middleware

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import morgan from "morgan";
+
+const app: express.Application = express();
+
+app.use(morgan("dev"));
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+);
+app.use(helmet());
+app.use(compression());
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
+app.use(express.json({ limit: "20mb" }));
+
+app.listen(3000, () => {
+	console.log("SERVER START");
+});
