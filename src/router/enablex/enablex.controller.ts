8c91fd153cd2d98a6ce4508e48d7ec01be94829d
Andy0414 <pjh8667@naver.com>
2020-07-19 10:39:51 +0900

    Update video

--- src/router/enablex/enablex.controller.ts
+++ src/router/enablex/enablex.controller.ts
@@ -19,30 +19,30 @@ class EnablexController extends Controller {
 	 * @param {NextFunction}next Express next
 	 */
 	public async createRoom(req: Request, res: Response, next: NextFunction) {
-		return res.json((await API.post("https://api.enablex.io/v1/rooms", req.body)).data);
+		return res.json((await API.post("https://api.enablex.io/video/v1/rooms", req.body)).data);
 	}
 	public async getRoomData(req: Request, res: Response, next: NextFunction) {
-		return res.json((await API.get("https://api.enablex.io/v1/rooms")).data);
+		return res.json((await API.get("https://api.enablex.io/video/v1/rooms")).data);
 	}
 	public async getRoomInfo(req: Request, res: Response, next: NextFunction) {
 		// get, req.params.room_id
-		return res.json((await API.get(`https://api.enablex.io/v1/rooms/${req.params.room_id}`)).data);
+		return res.json((await API.get(`https://api.enablex.io/video/v1/rooms/${req.params.room_id}`)).data);
 	}
 	public async updateRoom(req: Request, res: Response, next: NextFunction) {
 		// get, req.params.room_id
-		return res.json((await API.patch(`https://api.enablex.io/v1/rooms/${req.params.room_id}`), req.body).data);
+		return res.json((await API.patch(`https://api.enablex.io/video/v1/rooms/${req.params.room_id}`), req.body).data);
 	}
 	public async deleteRoom(req: Request, res: Response, next: NextFunction) {
 		// get, req.params.room_id
-		return res.json((await API.delete(`https://api.enablex.io/v1/rooms/${req.params.room_id}`)).data);
+		return res.json((await API.delete(`https://api.enablex.io/video/v1/rooms/${req.params.room_id}`)).data);
 	}
 	public async getUserInRoom(req: Request, res: Response, next: NextFunction) {
 		// get, req.params.room_id
-		return res.json((await API.get(`https://api.enablex.io/v1/rooms/${req.params.room_id}/users`)).data);
+		return res.json((await API.get(`https://api.enablex.io/video/v1/rooms/${req.params.room_id}/users`)).data);
 	}
 	public async createToken(req: Request, res: Response, next: NextFunction) {
 		// get, req.params.room_id
-		return res.json((await API.post(`https://api.enablex.io/v1/rooms/${req.params.room_id}/tokens`, req.body)).data);
+		return res.json((await API.post(`https://api.enablex.io/video/v1/rooms/${req.params.room_id}/tokens`, req.body)).data);
 	}
 }
 
